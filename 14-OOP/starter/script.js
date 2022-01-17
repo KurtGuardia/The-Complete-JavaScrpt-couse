fc31f83ec143f95d014b68d987694cc0f1c79fdc
Kurt Guardia <kurtguardia@gmail.com>
2022-01-17 11:20:46 +0100

    set up the Geolocation Map

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -4,7 +4,7 @@
 // Coding Challenge #1
 
 /* 
-1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+1. Use a constructor function to implement a Car. A car has a maker and a speed property. The speed property is the current speed of the car in km/h;
 2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
 3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
 4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
@@ -95,7 +95,7 @@ console.log(bmw.speedUS);
 // Coding Challenge #3
 
 /* 
-1. Use a constructor function to implement an Electric Car (called EV) as a CHILD "class" of Car. Besides a make and current speed, the EV also has the current battery charge in % ('charge' property);
+1. Use a constructor function to implement an Electric Car (called EV) as a CHILD "class" of Car. Besides a maker and current speed, the EV also has the current battery charge in % ('charge' property);
 2. Implement a 'chargeBattery' method which takes an argument 'chargeTo' and sets the battery charge to 'chargeTo';
 3. Implement an 'accelerate' method that will increase the car's speed by 20, and decrease the charge by 1%. Then log a message like this: 'Tesla going at 140 km/h, with a charge of 22%';
 4. Create an electric car object and experiment with calling 'accelerate', 'brake' and 'chargeBattery' (charge to 90%). Notice what happens when you 'accelerate'! HINT: Review the definiton of polymorphism 😉
@@ -105,23 +105,90 @@ DATA CAR 1: 'Tesla' going at 120 km/h, with a charge of 23%
 GOOD LUCK 😀
 */
 
-const EV = function (maker, speed, charge) {
-  Car.call(this, maker, speed);
-  this.charge = charge;
-};
+// const EV = function (maker, speed, charge) {
+//   Car.call(this, maker, speed);
+//   this.charge = charge;
+// };
 
-EV.prototype = Object.create(Car.prototype);
+// EV.prototype = Object.create(Car.prototype);
 
-EV.prototype.chargeTo = function (charge) {
-  this.charge = charge;
-};
+// EV.prototype.chargeTo = function (charge) {
+//   this.charge = charge;
+// };
 
-EV.prototype.accelerateEV = function () {
-  this.speed += 20;
-  this.charge--;
-  console.log(
-    `Your ${this.maker} is going ${this.speed} km/h and has ${this.charge}% of battery`
-  );
-};
+// EV.prototype.accelerateEV = function () {
+//   this.speed += 20;
+//   this.charge--;
+//   console.log(
+//     `Your ${this.maker} is going ${this.speed} km/h and has ${this.charge}% of battery`
+//   );
+// };
 
-const tesla = new EV('Tesla', 120, 23);
+// const tesla = new EV('Tesla', 120, 23);
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+1. Re-create challenge #3, but this time using ES6 classes: create an 'EVCl' child class of the 'CarCl' class
+2. Make the 'charge' property private;
+3. Implement the ability to chain the 'accelerate' and 'chargeBattery' methods of this class, and also update the 'brake' method in the 'CarCl' class. They experiment with chining!
+
+DATA CAR 1: 'Rivian' going at 120 km/h, with a charge of 23%
+
+GOOD LUCK 😀
+*/
+
+class CarCl {
+  constructor(maker, speed) {
+    this.maker = maker;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.maker} is going at ${this.speed} km/h`);
+  }
+
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.maker} is going at ${this.speed} km/h`);
+    return this;
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+class EVCl extends CarCl {
+  #charge;
+
+  constructor(maker, speed, charge) {
+    super(maker, speed), (this.#charge = charge);
+  }
+
+  accelerateEV() {
+    this.speed += 20;
+    this.#charge--;
+    console.log(
+      `Your ${this.maker} is going ${this.speed} km/h and has ${
+        this.#charge
+      }% of battery`
+    );
+    return this;
+  }
+
+  chargeTo(charge) {
+    this.#charge = charge;
+    return this;
+  }
+}
+
+const tesla = new EVCl('tesla', 120, 80);
+
+tesla.chargeTo(90).accelerateEV().brake();
